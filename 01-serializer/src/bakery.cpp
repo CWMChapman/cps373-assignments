5cfd768df23f65367d83ca01ffcf2697894b43a4
Christopher Chapman <cwmchapman@Christophers-Macbook-Pro.local>
2020-10-18 18:23:35 -0400

    the text serialization/deserialation works... the binary stuff not so much.

--- 01-serializer/src/bakery.cpp
+++ 01-serializer/src/bakery.cpp
@@ -1,6 +1,7 @@
 #include "bakery.hpp"
 #include <fstream>
 #include <iostream>
+#include <bitset>
 
 void print_bakery(const Bakery& bakery) {
   std::cout << "Employees: " << std::endl;
@@ -101,6 +102,205 @@ Bakery text_deserializer(std::string file_path) {
 
 // Implement these 3 functions!
 // Hint: use print_bakery to help think about the text serializer
-void text_serializer(const Bakery& bakery, std::string file_path);
-Bakery binary_deserializer(std::string file_path);
-void binary_serializer(const Bakery& bakery, std::string file_path);
+void text_serializer(const Bakery& bakery, std::string file_path) {
+  std::ofstream outfile (file_path);
+  outfile << "Employees: " << std::endl;
+  for (auto employee : bakery.employees) {
+    outfile << employee << std::endl;
+  }
+
+  outfile << std::endl;
+  outfile << "Items: " << std::endl;
+  for (auto item : bakery.items) {
+    outfile << item.name << ", " << item.price << std::endl;
+  }
+
+  outfile << std::endl;
+  outfile << "Orders: " << std::endl;
+  for (auto order : bakery.orders) {
+    outfile << order.employee << ": ";
+    auto j = 0;
+    for (auto item : order.items) {
+      outfile << item.second << " " << item.first;
+      j++;
+      if (size_t(j) < order.items.size())
+        outfile << ", ";
+    }
+    outfile << std::endl;
+  }
+
+  outfile.close();  
+}
+
+
+
+Bakery binary_deserializer(std::string file_path) {
+  std::ifstream infile(file_path);
+  std::string line;
+  Bakery bakery;
+
+  while (!infile.eof()) {
+    // Employees section
+    if (line.compare("@employees") == 0) {
+      std::getline(infile, line);
+      while (line.size() > 0) {
+        std::string binaryLine = "";
+        for (int i = 0; i < line.size(); ++i) {
+          binaryLine += std::bitset<8>(line[i]).to_string();
+        }
+        line = binaryLine;
+        bakery.employees.push_back(line);
+        std::getline(infile, line);
+      }
+    }
+
+    // Items section
+    if (line.compare("@items") == 0) {
+      std::getline(infile, line);
+      while (line.size() > 0) {
+        auto end = line.find(", ");
+        Item item;
+        item.name = line.substr(0, end);
+        std::string binaryItemName = "";
+        for (int i = 0; i < item.name.size(); ++i) {
+          binaryItemName += std::bitset<8>(item.name[i]).to_string();
+        }
+        item.name = binaryItemName;
+        
+        item.price = line.substr(end + 2);
+        std::string binaryItemPrice = "";
+        for (int i = 0; i < item.price.size(); ++i) {
+          binaryItemName += std::bitset<8>(item.price[i]).to_string();
+        }
+        item.price = binaryItemPrice;
+        bakery.items.push_back(item);
+        std::getline(infile, line);
+      }
+    }
+
+    // Orders section
+    if (line.compare("@orders") == 0) {
+      std::getline(infile, line);
+      auto e = bakery.employees;
+      while (line.size() > 0) {
+        Order order;
+        auto end = line.find(": ");
+        order.employee = line.substr(0, end);
+
+        // Find all the orders
+        auto rest = line.substr(end + 2);
+        size_t pos = 0;
+        std::string token;
+        while ((pos = rest.find(", ")) != std::string::npos) {
+          token = rest.substr(0, pos);
+          end = token.find(" ");
+          auto quantity = token.substr(0, end);
+          std::string binaryQuantity = "";
+          for (int i = 0; i < quantity.size(); ++i) {
+            binaryQuantity += std::bitset<8>(quantity[i]).to_string();
+          }
+          quantity = binaryQuantity;
+
+          auto item_name = token.substr(end + 1);
+          std::string binaryItemName = "";
+          for (int i = 0; i < item_name.size(); ++i) {
+            binaryItemName += std::bitset<8>(item_name[i]).to_string();
+          }
+          item_name = binaryItemName;
+          order.items.push_back(std::make_pair(item_name, quantity));
+          rest.erase(0, pos + 2);
+        }
+        end = rest.find(" ");
+        auto quantity = rest.substr(0, end);
+          std::string binaryQuantity = "";
+          for (int i = 0; i < quantity.size(); ++i) {
+            binaryQuantity += std::bitset<8>(quantity[i]).to_string();
+          }
+          quantity = binaryQuantity;
+
+          auto item_name = rest.substr(end + 1);
+          std::string binaryItemName = "";
+          for (int i = 0; i < item_name.size(); ++i) {
+            binaryItemName += std::bitset<8>(item_name[i]).to_string();
+          }
+          item_name = binaryItemName;
+        order.items.push_back(std::make_pair(item_name, quantity));
+        bakery.orders.push_back(order);
+
+        // no more lines to read
+        if (infile.eof())
+          break;
+
+        std::getline(infile, line);
+      }
+    }
+
+    std::getline(infile, line);
+  }
+
+  return bakery;
+}
+
+
+
+void binary_serializer(const Bakery& bakery, std::string file_path) {
+  // std::ofstream outfile (file_path);
+  std::ofstream outfile;
+  outfile.open(file_path, std::ios::out | std::ios::binary);
+  // std::string out = "Employees: ";
+  // for (int i = 0; i < out.size(); ++i) {
+  //   outfile << std::bitset<8>(out[i]);
+  // }
+  // outfile << std::endl;
+  // outfile << "Employees: " << std::endl;
+  for (auto employee : bakery.employees) {
+    // std::
+    // employee = std::stoi(employee);
+    std::cout << (char*) &employee << std::endl;
+    outfile.write(employee.c_str(), employee.size());
+    // outfile << std::bitset<8>(std::string(employee)) << std::endl;
+  }
+
+  outfile << std::endl;
+  // out = "Items: ";
+  // for (int i = 0; i < out.size(); ++i) {
+  //   outfile << std::bitset<8>(out[i]);
+  // }
+  // outfile << std::endl;
+  // outfile << "Items: " << std::endl;
+  for (auto item : bakery.items) {
+    // outfile << std::bitset<8>(std::string(item.name)) << " " << std::bitset<8>(std::string(item.price)) << std::endl;
+    outfile.write(item.name.c_str(), item.name.size());
+    outfile.write(item.price.c_str(), item.price.size());
+  }
+
+  // outfile << std::endl;
+  // out = "Orders: ";
+  // for (int i = 0; i < out.size(); ++i) {
+  //   outfile << std::bitset<8>(out[i]);
+  // }
+  // outfile << "Orders: " << std::endl;
+  for (auto order : bakery.orders) {
+    std::string out = order.employee;
+    std::string outBinary = "";
+    for (int i = 0; i < out.size(); ++i) {
+      outBinary += std::bitset<8>(out[i]).to_string();
+    }
+    out = outBinary;
+    outfile.write(out.c_str(), out.size());
+    // outfile << order.employee << ": ";
+    auto j = 0;
+    for (auto item : order.items) {
+      // outfile << std::bitset<8>(std::string(item.second)) << " " << std::bitset<8>(std::string(item.first));
+      outfile.write(item.second.c_str(), item.second.size());
+    outfile.write(item.first.c_str(), item.first.size());
+      j++;
+      // if (size_t(j) < order.items.size())
+        // outfile << ", ";
+    }
+    // outfile << std::endl;
+  }
+
+  outfile.close();  
+
+}
--- 01-serializer/src/main.cpp
+++ 01-serializer/src/main.cpp
@@ -1,17 +1,66 @@
 #include <chrono>
 #include <iostream>
 #include "../include/bakery.hpp"
+
 using namespace std::chrono;
 
 int main() {
+
+  // Text Deserializer
   auto start = high_resolution_clock::now();
-  auto bakery = text_deserializer("../data/small.txt");
+  auto bakery = text_deserializer("../data/large.txt");
   auto deser_time = high_resolution_clock::now() - start;
   std::cout << "Text deserializer took: "
             << duration_cast<milliseconds>(deser_time).count() << "ms"
             << std::endl
             << std::endl;
-  print_bakery(bakery);
+  // print_bakery(bakery);
+
+
+  // Text Serializer
+  start = high_resolution_clock::now();
+  text_serializer(bakery, "../data/text_serialized.txt");
+  deser_time = high_resolution_clock::now() - start;
+  std::cout << "Text serializer took: "
+            << duration_cast<milliseconds>(deser_time).count() << "ms"
+            << std::endl
+            << std::endl;
+  // print_bakery(bakery);
+
+  
+  //Binary Deserializer
+  start = high_resolution_clock::now();
+  bakery = binary_deserializer("../data/large.txt");
+  deser_time = high_resolution_clock::now() - start;
+  std::cout << "Binary deserializer took: "
+            << duration_cast<milliseconds>(deser_time).count() << "ms"
+            << std::endl
+            << std::endl;
+  // print_bakery(bakery);
+
+  //Binary Serializer
+  start = high_resolution_clock::now();
+  binary_serializer(bakery, "../data/binary_serialized.txt");
+  deser_time = high_resolution_clock::now() - start;
+  std::cout << "Binary serializer took: "
+            << duration_cast<milliseconds>(deser_time).count() << "ms"
+            << std::endl
+            << std::endl;
+  // print_bakery(bakery);
+  
+
 
   return 0;
 }
+
+
+
+/*
+Questions:
+
+1. Am I storing the values correctly in binary deserialization?
+2. How do I 
+
+
+
+*/
