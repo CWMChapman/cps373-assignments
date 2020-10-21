e1c0402226c5f8b6c946d268b5ca432469997dae
Christopher Chapman <cwmchapman@Christophers-Macbook-Pro.local>
2020-10-20 23:59:58 -0400

    cleaned up

--- 01-serializer/src/bakery.cpp
+++ 01-serializer/src/bakery.cpp
@@ -244,88 +244,39 @@ Bakery binary_deserializer(std::string file_path) {
 
 
 void binary_serializer(const Bakery& bakery, std::string file_path) {
-  // std::ofstream outfile (file_path);
   std::ofstream outfile;
   outfile.open(file_path, std::ios::out | std::ios::binary);
-  // std::string out = "Employees: ";
-  // for (int i = 0; i < out.size(); ++i) {
-  //   outfile << std::bitset<8>(out[i]);
-  // }
-  // outfile << std::endl;
-  // outfile << "Employees: " << std::endl;
+
   for (auto employee : bakery.employees) {
     std::string out = "";
     for (int i = 0; i < employee.size(); ++i) {
-      // out += std::bitset<8>(employee[i]).to_string();
       outfile << std::bitset<8>(employee[i]);
     }
-    // std::
-    // employee = std::stoi(employee);
-    // std::cout << (char*) &employee << std::endl;
-    // outfile.write(out.c_str(), out.size());
-    // outfile << std::bitset<8>(std::string(employee)) << std::endl;
   }
 
-  // outfile << std::endl;
-  // out = "Items: ";
-  // for (int i = 0; i < out.size(); ++i) {
-  //   outfile << std::bitset<8>(out[i]);
-  // }
-  // outfile << std::endl;
-  // outfile << "Items: " << std::endl;
   for (auto item : bakery.items) {
-    // outfile << std::bitset<8>(std::string(item.name)) << " " << std::bitset<8>(std::string(item.price)) << std::endl;
-    std::string out_name = "";
-    std::string out_price = "";
     for (int i = 0; i < item.name.size(); ++i) {
-      // out_name += std::bitset<8>(item.name[i]).to_string();
       outfile <<  std::bitset<8>(item.name[i]);
     }
     for (int i = 0; i < item.price.size(); ++i) {
-      // out_price += std::bitset<8>(item.price[i]).to_string();
       outfile << std::bitset<8>(item.price[i]);
     }
-    // outfile.write(out_name.c_str(), out_name.size());
-    // outfile.write(out_price.c_str(), out_price.size());
   }
 
-  // outfile << std::endl;
-  // out = "Orders: ";
-  // for (int i = 0; i < out.size(); ++i) {
-  //   outfile << std::bitset<8>(out[i]);
-  // }
-  // outfile << "Orders: " << std::endl;
   for (auto order : bakery.orders) {
-    std::string out_employee = "";
     for (int i = 0; i < order.employee.size(); ++i) {
-      // out_employee += std::bitset<8>(order.employee[i]).to_string();
       outfile << std::bitset<8>(order.employee[i]);
     }
-    outfile.write(out_employee.c_str(), out_employee.size());
     auto j = 0;
     for (auto item : order.items) {
-      // outfile << std::bitset<8>(std::string(item.second)) << " " << std::bitset<8>(std::string(item.first));
-      std::string out_first = "";
-      std::string out_second = "";
       for (int i = 0; i < item.first.size(); ++i) {
-        // out_first += std::bitset<8>(item.first[i]).to_string();
         outfile << std::bitset<8>(item.first[i]);
       }
       for (int i = 0; i < item.second.size(); ++i) {
-        // out_second += std::bitset<8>(item.second[i]).to_string();
         outfile << std::bitset<8>(item.second[i]);
       }
-      // std::cout << out_second << "," << out_second.size() << std::endl;
-      // std::cout << std::stoi("01001011") << std::endl;
-      // outfile << std::stoi(out_second);
-      // outfile << std::stoi(out_first;
-      // outfile.write(out_second.c_str(), out_second.size());
-      // outfile.write(out_first.c_str(), out_first.size());
       j++;
-      // if (size_t(j) < order.items.size())
-        // outfile << ", ";
     }
-    // outfile << std::endl;
   }
 
   outfile.close();  
