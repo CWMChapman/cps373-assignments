36bcb86506731b42749b08a76dd92e295dff9cad
Christopher Chapman <cwmchapman@gmail.com>
2020-11-03 12:47:33 -0500

    adding timeouts for each packet, but the server still segfaults sometimes... # of packet re-requests < 1000

--- 03-reliable/src/client.cpp
+++ 03-reliable/src/client.cpp
@@ -3,10 +3,9 @@
 #include <fstream>
 #include "../include/sender.hpp"
 #include <queue>
+#include <chrono>
 
-
-// #define NUM_MSGS 852 // this is defined in sender.hpp
-
+namespace sc = std::chrono;
 using asio::ip::tcp;
 
 
@@ -15,19 +14,25 @@ int main() {
   Sender sender(io_context, "127.0.0.1", "3000");
 
   std::queue <int> q;
+  std::queue <sc::milliseconds> timeout;
+  
   int message_counter = 0;
-
   int32_t curr_msg = 0;
-
-  std::array<std::string, NUM_MSGS> message = {""};
-  int queue_pop_counter = 0;
+  int rerequest_counter = 0;
   int while_counter = 0;
+  
+  std::array<std::string, NUM_MSGS> message;
+
+  auto start = sc::high_resolution_clock::now();
   while (true) {
     if (q.size() < 10 && curr_msg < NUM_MSGS){ 
       int c = curr_msg;
       if (c + 20 <= NUM_MSGS) {
         for (int i = c; i < c + 20; i++) {
           sender.request_msg(i);
+          auto hb = sc::duration_cast<sc::milliseconds>(sc::high_resolution_clock::now().time_since_epoch());
+          hb += sc::milliseconds(500);   
+          timeout.push(hb);
           q.push(i);
           curr_msg++;
         }
@@ -35,15 +40,18 @@ int main() {
       else {
         for (int i = c; i < NUM_MSGS; i++) {
           sender.request_msg(i);
+          auto hb = sc::duration_cast<sc::milliseconds>(sc::high_resolution_clock::now().time_since_epoch());
+          hb += sc::milliseconds(500);   
+          timeout.push(hb);
           q.push(i);
           curr_msg++;
         }
       }
     }
 
-    std::cout << "While counter: " << while_counter << ", queue_pop_counter: " << queue_pop_counter << ", message_counter: " << message_counter << ", curr_msg: " << curr_msg << ", size of queue: " << q.size() << std::endl;
+    std::cout << "While counter: " << while_counter << ", # of packets re-requested: " << rerequest_counter << ", message_counter: " << message_counter << ", curr_msg: " << curr_msg << ", size of queue: " << q.size() << std::endl;
 
-    if (sender.data_ready()) {
+    while (sender.data_ready()) {
       auto msg = sender.get_msg();
       auto data_str = std::string(msg.data.data(), CHUNK_SIZE);
 
@@ -53,25 +61,34 @@ int main() {
       }
     }
     
-    else {
-      while(!q.empty() && !message[q.front()].empty() ) {
-        q.pop();
-      }
-      // std::cout << "curr_msg: " << curr_msg << ", message_counter: " << message_counter << ", curr_msg / (message_counter + 1): " << curr_msg / (message_counter + 1) << std::endl;
-      if (!q.empty()){
-        q.push(q.front());
-        sender.request_msg(q.front());
-        q.pop();
-        queue_pop_counter++;
-      }
+    while(!q.empty() && !message[q.front()].empty()) {
+      timeout.pop();
+      q.pop();
     }
+    if (!q.empty() && timeout.front() < sc::duration_cast<sc::milliseconds>(
+        sc::high_resolution_clock::now().time_since_epoch())){
+
+      auto hb = sc::duration_cast<sc::milliseconds>(sc::high_resolution_clock::now().time_since_epoch());
+      hb += sc::milliseconds(500);   
+      
+      sender.request_msg(q.front());
+      timeout.pop();
+      timeout.push(hb);
+      q.push(q.front());
+      q.pop();
+      rerequest_counter++;
+    }
+
+    while_counter++;
 
     if (NUM_MSGS == curr_msg && NUM_MSGS == message_counter)
       break;
-    while_counter++;
-  }
 
-  std::cout << "queue pop counter is: " << queue_pop_counter << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+  auto deser_time = sc::high_resolution_clock::now() - start;
+
+  std::cout << "# of packets re-requested is: " << rerequest_counter << std::endl;
 
   std::ofstream outfile ("../data/poem.txt");
   std::cout << std::endl;
@@ -80,6 +97,9 @@ int main() {
   }
   outfile << std::endl;
   outfile.close();
+
+  std::cout << "All packets have been recieved, and poem is in the data dir." << std::endl;
+  std::cout << "Took: " << sc::duration_cast<sc::seconds>(deser_time).count() << "s" << std::endl;
 
   
 
